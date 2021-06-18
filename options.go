afea9b73b39a4cbc5f1bfe22a1176515d2836f9a
Christian Morbidelli <cmorbidelli@protonmail.com>
2021-06-18 15:25:41 -0400

    ran go fmt

--- options.go
+++ options.go
@@ -1,10 +1,10 @@
 package textwrap
 
 // options allow the user to override a TextWrapper's default values.
-// The function calls can be passed as arguments to NewTextWrapper, 
+// The function calls can be passed as arguments to NewTextWrapper,
 // Wrap, Fill, and Shorten.  For example:
 //     Fill(myText, Width(50), FixSentenceEndings(true))
-// fills myText using a TextWrapper with the default Width and 
+// fills myText using a TextWrapper with the default Width and
 // FixSentenceEndings values replaced by the user-provided values.
 // One option corresponds to each exported TextWrapper field.
 type option func(*TextWrapper)
@@ -12,9 +12,9 @@ type option func(*TextWrapper)
 // A call to Width may be passed to NewTextWrapper or any wrapping
 // function to override the default line width (70).
 func Width(i int) option {
-    return func(t *TextWrapper) {
-        t.Width = i
-    }
+	return func(t *TextWrapper) {
+		t.Width = i
+	}
 }
 
 // A call to ExpandTabs may be passed to NewTextWrapper or any
@@ -22,9 +22,9 @@ func Width(i int) option {
 // ExpandTabs to Shorten has no effect, as each sequence of
 // whitespace is replaced by a single space before wrapping.
 func ExpandTabs(b bool) option {
-    return func(t *TextWrapper) {
-        t.ExpandTabs = b
-    }
+	return func(t *TextWrapper) {
+		t.ExpandTabs = b
+	}
 }
 
 // A call to TabSize may be passed to NewTextWrapper or any wrapping
@@ -32,9 +32,9 @@ func ExpandTabs(b bool) option {
 // passing TabSize to Shorten has no effect because each sequence of
 // whitespace is replaced by a single space before wrapping.
 func TabSize(i int) option {
-    return func(t *TextWrapper) {
-        t.TabSize = i
-    }
+	return func(t *TextWrapper) {
+		t.TabSize = i
+	}
 }
 
 // A call to ReplaceWhitespace may be passed to NewTextWrapper or any
@@ -42,57 +42,57 @@ func TabSize(i int) option {
 // ReplaceWhitespace to Shorten has no effect, as each sequence of
 // whitespace is replaced by a single space before wrapping.
 func ReplaceWhitespace(b bool) option {
-    return func(t *TextWrapper) {
-        t.ReplaceWhitespace = b
-    }
+	return func(t *TextWrapper) {
+		t.ReplaceWhitespace = b
+	}
 }
 
 // A call to DropWhitespace may be passed to NewTextWrapper or any
 // wrapping function to override the default value (true).
 func DropWhitespace(b bool) option {
-    return func(t *TextWrapper) {
-        t.DropWhitespace = b
-    }
+	return func(t *TextWrapper) {
+		t.DropWhitespace = b
+	}
 }
 
 // A call to InitialIndent may be passed to NewTextWrapper or any
 // wrapping function to override the default initial indent ("").
 func InitialIndent(s string) option {
-    return func(t *TextWrapper) {
-        t.InitialIndent = s
-    }
+	return func(t *TextWrapper) {
+		t.InitialIndent = s
+	}
 }
 
 // A call to SubsequentIndent may be passed to NewTextWrapper or any
 // wrapping function to override the default subsequent indent ("").
 func SubsequentIndent(s string) option {
-    return func(t *TextWrapper) {
-        t.SubsequentIndent = s
-    }
+	return func(t *TextWrapper) {
+		t.SubsequentIndent = s
+	}
 }
 
 // A call to FixSentenceEndings may be passed to NewTextWrapper or
 // any wrapping function to override the default value (false).
 func FixSentenceEndings(b bool) option {
-    return func(t *TextWrapper) {
-        t.FixSentenceEndings = b
-    }
+	return func(t *TextWrapper) {
+		t.FixSentenceEndings = b
+	}
 }
 
 // A call to BreakLongWords may be passed to NewTextWrapper or any
 // wrapping function to override the default value (true).
 func BreakLongWords(b bool) option {
-    return func(t *TextWrapper) {
-        t.BreakLongWords = b
-    }
+	return func(t *TextWrapper) {
+		t.BreakLongWords = b
+	}
 }
 
 // A call to BreakOnHyphens may be passed to NewTextWrapper or any
 // wrapping function to override the default value (true).
 func BreakOnHyphens(b bool) option {
-    return func(t *TextWrapper) {
-        t.BreakOnHyphens = b
-    }
+	return func(t *TextWrapper) {
+		t.BreakOnHyphens = b
+	}
 }
 
 // A call to MaxLines may be passed to NewTextWrapper or any wrapping
@@ -101,15 +101,15 @@ func BreakOnHyphens(b bool) option {
 // MaxLines to Shorten has no effect, as Shorten always returns a
 // single line.
 func MaxLines(i int) option {
-    return func(t *TextWrapper) {
-        t.MaxLines = i
-    }
+	return func(t *TextWrapper) {
+		t.MaxLines = i
+	}
 }
 
 // A call to Placeholder may be passed to NewTextWrapper or any
 // wrapping function to override the default placeholder (" [...]").
 func Placeholder(s string) option {
-    return func(t *TextWrapper) {
-        t.Placeholder = s
-    }
+	return func(t *TextWrapper) {
+		t.Placeholder = s
+	}
 }
--- textwrapper.go
+++ textwrapper.go
@@ -19,113 +19,113 @@
 package textwrap
 
 import (
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 )
 
-// The following variables define whitespace and regexps that are 
+// The following variables define whitespace and regexps that are
 // used throughout the package.  Although it is not recommended, they
-// can be set to change the functions' behaviors.  This may allow, 
+// can be set to change the functions' behaviors.  This may allow,
 // for instance, textwrap to be adapted for different character sets.
 var (
-    Space            = " "
-    Tab              = "\t"
-    Newline          = "\n"
-    OtherWhitespace  = Tab + Newline + "\v\f\r"
-    Whitespace       = Space + OtherWhitespace
+	Space           = " "
+	Tab             = "\t"
+	Newline         = "\n"
+	OtherWhitespace = Tab + Newline + "\v\f\r"
+	Whitespace      = Space + OtherWhitespace
 
-    // Note: per the regexp package documentation, Regexp structs are
-    // safe for concurrent use by multiple goroutines.
+	// Note: per the regexp package documentation, Regexp structs are
+	// safe for concurrent use by multiple goroutines.
 
-    // WhitespaceRe matches any whitespace character except Space.  
-    // It is used to replace characters with spaces if 
-    // ReplaceWhitespaceRe is true.
-    WhitespaceRe     = regexp.MustCompile("[" + OtherWhitespace + "]")
-    // SentenceEndingRe matches any non-whitespace character, followed
-    // by a sentence-ending punctuation mark and at least one space
-    // It is only used if FixSentenceEndings is true.
-    SentenceEndingRe = regexp.MustCompile("([^" + Whitespace + "]" +
-                                          "[.!?]['\"]?) [ ]*")
-    // ChunksHyphenRe is used to break text into chunks for wrapping if
-    // BreakOnHyphens is true.
-    ChunksHyphenRe   = regexp.MustCompile("(\u2014|[^" + Whitespace + "]+-|" +
-                                          "[^" + Whitespace +
-                                          "]+|[" + Whitespace + "]+)")
-    // ChunksNoHyphenRe is used if BreakOnHyphens is false.
-    ChunksNoHyphenRe = regexp.MustCompile("(\u2014|[^" + Whitespace + "]+|" +
-                                          "[" + Whitespace + "]+)")
-    // ConsWhitespaceRe is used by Shorten to find consecutive
-    // whitespace characters.
-    ConsWhitespaceRe = regexp.MustCompile("[" + Whitespace + "]+")
-    // LeadWhitespaceRe is used by Dedent to find leading whitespace.
-    LeadWhitespaceRe = regexp.MustCompile("^[" + Whitespace + "]*")
+	// WhitespaceRe matches any whitespace character except Space.
+	// It is used to replace characters with spaces if
+	// ReplaceWhitespaceRe is true.
+	WhitespaceRe = regexp.MustCompile("[" + OtherWhitespace + "]")
+	// SentenceEndingRe matches any non-whitespace character, followed
+	// by a sentence-ending punctuation mark and at least one space
+	// It is only used if FixSentenceEndings is true.
+	SentenceEndingRe = regexp.MustCompile("([^" + Whitespace + "]" +
+		"[.!?]['\"]?) [ ]*")
+	// ChunksHyphenRe is used to break text into chunks for wrapping if
+	// BreakOnHyphens is true.
+	ChunksHyphenRe = regexp.MustCompile("(\u2014|[^" + Whitespace + "]+-|" +
+		"[^" + Whitespace +
+		"]+|[" + Whitespace + "]+)")
+	// ChunksNoHyphenRe is used if BreakOnHyphens is false.
+	ChunksNoHyphenRe = regexp.MustCompile("(\u2014|[^" + Whitespace + "]+|" +
+		"[" + Whitespace + "]+)")
+	// ConsWhitespaceRe is used by Shorten to find consecutive
+	// whitespace characters.
+	ConsWhitespaceRe = regexp.MustCompile("[" + Whitespace + "]+")
+	// LeadWhitespaceRe is used by Dedent to find leading whitespace.
+	LeadWhitespaceRe = regexp.MustCompile("^[" + Whitespace + "]*")
 )
 
 // functions to simplify stripping whitespace from chunks of text
 func strip(s string) string {
-    return strings.Trim(s, Whitespace)
+	return strings.Trim(s, Whitespace)
 }
 
 func lStrip(s string) string {
-    return strings.TrimLeft(s, Whitespace)
+	return strings.TrimLeft(s, Whitespace)
 }
 
 func rStrip(s string) string {
-    return strings.TrimRight(s, Whitespace)
+	return strings.TrimRight(s, Whitespace)
 }
 
 // TextWrapper contains values that govern wrapping behavior.
 type TextWrapper struct {
-    // Line width.  Default value is 70.
-    Width              int
+	// Line width.  Default value is 70.
+	Width int
 
-    // If true, tab characters are replaced with TabSize number of
-    // spaces before wrapping.  Default value is true.
-    ExpandTabs         bool
+	// If true, tab characters are replaced with TabSize number of
+	// spaces before wrapping.  Default value is true.
+	ExpandTabs bool
 
-    // Tab size in spaces.  Default value is 8 spaces.
-    TabSize            int
+	// Tab size in spaces.  Default value is 8 spaces.
+	TabSize int
 
-    // If true, each whitespace character is replaced with a space.
-    // Whitespace characters are defined as: '\t', '\n', '\v' '\f', 
-    // '\r', and ' '.  Default value is true.
-    ReplaceWhitespace  bool
+	// If true, each whitespace character is replaced with a space.
+	// Whitespace characters are defined as: '\t', '\n', '\v' '\f',
+	// '\r', and ' '.  Default value is true.
+	ReplaceWhitespace bool
 
-    // If true, leading and trailing whitespace is dropped from each
-    // line, after wrapping but before indenting.  Whitespace-only
-    // lines are dropped.  Ignores whitespace at the beginning of 
-    // text if followed by non-whitespace.  Default value is true.
-    DropWhitespace     bool
+	// If true, leading and trailing whitespace is dropped from each
+	// line, after wrapping but before indenting.  Whitespace-only
+	// lines are dropped.  Ignores whitespace at the beginning of
+	// text if followed by non-whitespace.  Default value is true.
+	DropWhitespace bool
 
-    // Prepended to the first line of text.  Default value is "".
-    InitialIndent      string
+	// Prepended to the first line of text.  Default value is "".
+	InitialIndent string
 
-    // Prepended to lines following the first.  Default value is "".
-    SubsequentIndent   string
+	// Prepended to lines following the first.  Default value is "".
+	SubsequentIndent string
 
-    // Attempts to place two spaces after the end of each sentence
-    // using the sentenceEnding regexp.  Unfortunately, the regexp
-    // can't currently distinguish punctuation within a sentence from
-    // sentence endings, so (for instance) it will also match with
-    // "Mr. Rogers".  Default is false.
-    FixSentenceEndings bool
+	// Attempts to place two spaces after the end of each sentence
+	// using the sentenceEnding regexp.  Unfortunately, the regexp
+	// can't currently distinguish punctuation within a sentence from
+	// sentence endings, so (for instance) it will also match with
+	// "Mr. Rogers".  Default is false.
+	FixSentenceEndings bool
 
-    // If true, words too long to fit on the line will be broken.  
-    // Otherwise, they will be placed on a separate line.  Default
-    // value is true.
-    BreakLongWords     bool
+	// If true, words too long to fit on the line will be broken.
+	// Otherwise, they will be placed on a separate line.  Default
+	// value is true.
+	BreakLongWords bool
 
-    // Allows wrapping to occur on hyphens.  Default value is true.
-    BreakOnHyphens     bool
+	// Allows wrapping to occur on hyphens.  Default value is true.
+	BreakOnHyphens bool
 
-    // If greater than zero, will limit output to MaxLines lines.
-    // Default value is 0.
-    MaxLines           int
+	// If greater than zero, will limit output to MaxLines lines.
+	// Default value is 0.
+	MaxLines int
 
-    // If MaxLines is greater than zero and the text has to be
-    // truncated, the last line will end with Placeholder.  Default
-    // value is " [...]".
-    Placeholder        string
+	// If MaxLines is greater than zero and the text has to be
+	// truncated, the last line will end with Placeholder.  Default
+	// value is " [...]".
+	Placeholder string
 }
 
 // NewTextWrapper returns a TextWrapper struct. Each field receives a
@@ -136,175 +136,175 @@ type TextWrapper struct {
 //
 // sets t.Width and t.TabSize, but otherwise keeps the default values.
 func NewTextWrapper(opts ...option) TextWrapper {
-    t := TextWrapper{
-        Width:              70,
-        ExpandTabs:         true,
-        TabSize:            8,
-        ReplaceWhitespace:  true,
-        DropWhitespace:     true,
-        InitialIndent:      "",
-        SubsequentIndent:   "",
-        FixSentenceEndings: false,
-        BreakLongWords:     true,
-        BreakOnHyphens:     true,
-        MaxLines:           0,
-        Placeholder:        " [...]",
-    }
+	t := TextWrapper{
+		Width:              70,
+		ExpandTabs:         true,
+		TabSize:            8,
+		ReplaceWhitespace:  true,
+		DropWhitespace:     true,
+		InitialIndent:      "",
+		SubsequentIndent:   "",
+		FixSentenceEndings: false,
+		BreakLongWords:     true,
+		BreakOnHyphens:     true,
+		MaxLines:           0,
+		Placeholder:        " [...]",
+	}
 
-    for _, opt := range opts {
-        opt(&t)
-    }
+	for _, opt := range opts {
+		opt(&t)
+	}
 
-    return t
+	return t
 }
 
 // Wrap splits text into lines of specified length.  The TextWrapper
 // object contains fields that can be modified to control Wrap's
 // behavior.  See TextWrapper for descriptions of the fields.
 func (t *TextWrapper) Wrap(text string) []string {
-    // First, Wrap checks if the values of TextWrapper's fields 
-    // make it impossible to wrap the text.  This can occur if:
-    // (1) the line width is less than 1; 
-    if t.Width < 1 {
-        panic("Width must be at least 1.")
+	// First, Wrap checks if the values of TextWrapper's fields
+	// make it impossible to wrap the text.  This can occur if:
+	// (1) the line width is less than 1;
+	if t.Width < 1 {
+		panic("Width must be at least 1.")
 
-    // (2) ExpandTabs is true and TabSize is less than zero; or
-    } else if t.ExpandTabs && t.TabSize < 0 {
-        panic("Tab size must be at least 0 to expand tabs.")
-    }
+		// (2) ExpandTabs is true and TabSize is less than zero; or
+	} else if t.ExpandTabs && t.TabSize < 0 {
+		panic("Tab size must be at least 0 to expand tabs.")
+	}
 
-    // (3) MaxLines is positive, but the last line is not wide enough
-    //     to hold both the indent and the placeholder.
-    if t.MaxLines > 0 {
-        indent := t.SubsequentIndent
-        if t.MaxLines == 1 {
-            indent = t.InitialIndent
-        }
+	// (3) MaxLines is positive, but the last line is not wide enough
+	//     to hold both the indent and the placeholder.
+	if t.MaxLines > 0 {
+		indent := t.SubsequentIndent
+		if t.MaxLines == 1 {
+			indent = t.InitialIndent
+		}
 
-        if len(indent) + len(lStrip(t.Placeholder)) > t.Width {
-            panic("Placeholder is too wide to fit on indented line.")
-        }
-    }
-    // If one of these conditions is met, Wrap panics instead  of
-    // restoring the default values because it is difficult to infer
-    // the user's intent, and more likely that a mistake occured.
+		if len(indent)+len(lStrip(t.Placeholder)) > t.Width {
+			panic("Placeholder is too wide to fit on indented line.")
+		}
+	}
+	// If one of these conditions is met, Wrap panics instead  of
+	// restoring the default values because it is difficult to infer
+	// the user's intent, and more likely that a mistake occured.
 
-    // expands tabs if ExpandTabs is true
-    if t.ExpandTabs {
-        tabString := strings.Repeat(Space, t.TabSize)
-        text = strings.Replace(text, Tab, tabString, -1)
-    }
+	// expands tabs if ExpandTabs is true
+	if t.ExpandTabs {
+		tabString := strings.Repeat(Space, t.TabSize)
+		text = strings.Replace(text, Tab, tabString, -1)
+	}
 
-    // replaces whitespace if ReplaceWhitespace is true
-    if t.ReplaceWhitespace {
-        text = WhitespaceRe.ReplaceAllString(text, Space)
-    }
+	// replaces whitespace if ReplaceWhitespace is true
+	if t.ReplaceWhitespace {
+		text = WhitespaceRe.ReplaceAllString(text, Space)
+	}
 
-    // attempts to fix sentence endings if FixSentenceEndings is true
-    if t.FixSentenceEndings {
-        text = SentenceEndingRe.ReplaceAllString(text, "${1}" + Space + Space)
-    }
+	// attempts to fix sentence endings if FixSentenceEndings is true
+	if t.FixSentenceEndings {
+		text = SentenceEndingRe.ReplaceAllString(text, "${1}"+Space+Space)
+	}
 
-    // breaks text into chunks depending on BreakOnHyphens
-    var chunks []string
-    if t.BreakOnHyphens {
-        chunks = ChunksHyphenRe.FindAllString(text, -1)
-    } else {
-        chunks = ChunksNoHyphenRe.FindAllString(text, -1)
-    }
+	// breaks text into chunks depending on BreakOnHyphens
+	var chunks []string
+	if t.BreakOnHyphens {
+		chunks = ChunksHyphenRe.FindAllString(text, -1)
+	} else {
+		chunks = ChunksNoHyphenRe.FindAllString(text, -1)
+	}
 
-    // iterates through lines
-    var lines []string
-    for i := 0; i < len(chunks); i++ {
-        // drops leading whitespace if DropWhitespace is true
-        if len(lines) > 0 && t.DropWhitespace && strip(chunks[i]) == "" {
-            i++
-        }
+	// iterates through lines
+	var lines []string
+	for i := 0; i < len(chunks); i++ {
+		// drops leading whitespace if DropWhitespace is true
+		if len(lines) > 0 && t.DropWhitespace && strip(chunks[i]) == "" {
+			i++
+		}
 
-        // selects appropriate indent
-        var indent string
-        if len(lines) > 0 {
-            indent = t.SubsequentIndent
-        } else {
-            indent = t.InitialIndent
-        }
+		// selects appropriate indent
+		var indent string
+		if len(lines) > 0 {
+			indent = t.SubsequentIndent
+		} else {
+			indent = t.InitialIndent
+		}
 
-        // sets line width to allow room for indent and placeholder
-        width := t.Width - len([]rune(indent))
-        if t.MaxLines > 0 && len(lines) == t.MaxLines - 1 {
-            width -= len(t.Placeholder)
-        }
+		// sets line width to allow room for indent and placeholder
+		width := t.Width - len([]rune(indent))
+		if t.MaxLines > 0 && len(lines) == t.MaxLines-1 {
+			width -= len(t.Placeholder)
+		}
 
-        // appends chunks to current line until the next chunk would
-        // exceed width, or text ends
-        var curLen int
-        var curLine []string
-        for ; i < len(chunks); i++ {
-            if curLen + len([]rune(chunks[i])) >= width {
-                i--
-                break
-            }
-            curLine = append(curLine, chunks[i])
-            curLen += len([]rune(chunks[i]))
-        }
+		// appends chunks to current line until the next chunk would
+		// exceed width, or text ends
+		var curLen int
+		var curLine []string
+		for ; i < len(chunks); i++ {
+			if curLen+len([]rune(chunks[i])) >= width {
+				i--
+				break
+			}
+			curLine = append(curLine, chunks[i])
+			curLen += len([]rune(chunks[i]))
+		}
 
-        // peeks ahead to check if next chunk will need to be split
-        // or placed on its own line
-        if i + 1 < len(chunks) && len([]rune(chunks[i+1])) > width {
-            // if BreakLongWords is true, appends as much of the
-            // chunk as possible to the current line, and leaves any
-            // remainder for the next line 
-            if t.BreakLongWords {
-                c := []rune(chunks[i + 1])
-                spaceLeft := 1
-                if width >= 1 {
-                    spaceLeft = width - curLen
-                }
-                curLine = append(curLine, string(c[:spaceLeft]))
-                curLen += spaceLeft
-                chunks[i + 1] = string(c[spaceLeft:])
-            // or, if current line is empty, the chunk is appended
-            } else if curLen == 0 && len(lines) != t.MaxLines - 1 {
-                i++
-                curLine = append(curLine, chunks[i])
-                curLen += len([]rune(chunks[i]))
-            }
-        }
+		// peeks ahead to check if next chunk will need to be split
+		// or placed on its own line
+		if i+1 < len(chunks) && len([]rune(chunks[i+1])) > width {
+			// if BreakLongWords is true, appends as much of the
+			// chunk as possible to the current line, and leaves any
+			// remainder for the next line
+			if t.BreakLongWords {
+				c := []rune(chunks[i+1])
+				spaceLeft := 1
+				if width >= 1 {
+					spaceLeft = width - curLen
+				}
+				curLine = append(curLine, string(c[:spaceLeft]))
+				curLen += spaceLeft
+				chunks[i+1] = string(c[spaceLeft:])
+				// or, if current line is empty, the chunk is appended
+			} else if curLen == 0 && len(lines) != t.MaxLines-1 {
+				i++
+				curLine = append(curLine, chunks[i])
+				curLen += len([]rune(chunks[i]))
+			}
+		}
 
-        // if DropWhitespace is true, drops any trailing whitespace
-        if last := len(curLine) - 1; t.DropWhitespace &&
-           curLen > 0 && strip(curLine[last]) == "" {
-            curLen -= len([]rune(curLine[last]))
-            curLine = curLine[:last]
-        }
+		// if DropWhitespace is true, drops any trailing whitespace
+		if last := len(curLine) - 1; t.DropWhitespace &&
+			curLen > 0 && strip(curLine[last]) == "" {
+			curLen -= len([]rune(curLine[last]))
+			curLine = curLine[:last]
+		}
 
-        // if the current line is MaxLine, applies any placeholder
-        // and indent, appends the current line to lines, and exits
-        // the main loop
-        if t.MaxLines > 0 && len(lines) == t.MaxLines - 1 {
-            // if the line is empty, removes any leading whitespace
-            // from the placeholder
-            if curLen == 0 {
-                curLine = append(curLine, lStrip(t.Placeholder))
-            } else {
-                curLine = append(curLine, t.Placeholder)
-            }
-            lines = append(lines, indent + strings.Join(curLine, ""))
-            break
-        // or, if the current line is not empty, applies any indent
-        // and appends the current line to lines
-        } else if curLen > 0 {
-            lines = append(lines, indent + strings.Join(curLine, ""))
-        }
-    }
+		// if the current line is MaxLine, applies any placeholder
+		// and indent, appends the current line to lines, and exits
+		// the main loop
+		if t.MaxLines > 0 && len(lines) == t.MaxLines-1 {
+			// if the line is empty, removes any leading whitespace
+			// from the placeholder
+			if curLen == 0 {
+				curLine = append(curLine, lStrip(t.Placeholder))
+			} else {
+				curLine = append(curLine, t.Placeholder)
+			}
+			lines = append(lines, indent+strings.Join(curLine, ""))
+			break
+			// or, if the current line is not empty, applies any indent
+			// and appends the current line to lines
+		} else if curLen > 0 {
+			lines = append(lines, indent+strings.Join(curLine, ""))
+		}
+	}
 
-    return lines
+	return lines
 }
 
 // Fill wraps the text and returns a single string consisting of
 // the newline-separated lines.  The TextWrapper object contains
-// fields that can be modified to control Wrap's behavior.  See 
+// fields that can be modified to control Wrap's behavior.  See
 // TextWrapper for descriptions of the fields.
 func (t *TextWrapper) Fill(text string) string {
-    return strings.Join(t.Wrap(text), Newline)
+	return strings.Join(t.Wrap(text), Newline)
 }
